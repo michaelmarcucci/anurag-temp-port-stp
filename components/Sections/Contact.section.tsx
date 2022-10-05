ec5828481e4ed900ec5ee18a38933d8218045758
michaelmarcucci <64811901+michaelmarcucci@users.noreply.github.com>
2022-10-05 15:34:56 -0400

    current resume add

--- components/Sections/Contact.section.tsx
+++ components/Sections/Contact.section.tsx
@@ -1,39 +1,46 @@
 import {
-  BsSpotify,
-  MdEmail,
-  MdFilePresent,
-  AiOutlineGithub,
-  AiOutlineTwitter,
-  FaLinkedinIn
-} from "../Misc/Icons.collection";
+	BsSpotify,
+	MdEmail,
+	MdFilePresent,
+	AiOutlineGithub,
+	AiOutlineTwitter,
+	FaLinkedinIn,
+} from '../Misc/Icons.collection';
 
-import { Icon } from "../Misc/Icon.component";
+import { Icon } from '../Misc/Icon.component';
 
 const Contact = () => {
-  return (
-    <div className="font-sen px-3" id="contact">
-      <p className="text-3xl font-bold text-fourth">Contact Me</p>
+	return (
+		<div className='px-3 font-sen' id='contact'>
+			<p className='text-3xl font-bold text-fourth'>Contact Me</p>
 
-      <div className="my-8 flex flex-row justify-center gap-x-4">
-        <Icon icon={<AiOutlineGithub />} url="https://github.com/michaelmarcucci" />
+			<div className='my-8 flex flex-row justify-center gap-x-4'>
+				<Icon
+					icon={<AiOutlineGithub />}
+					url='https://github.com/michaelmarcucci'
+				/>
 
-        <Icon
-          icon={<AiOutlineTwitter />}
-          url="https://twitter.com/MichaelMarcucc9"
-        />
+				<Icon
+					icon={<AiOutlineTwitter />}
+					url='https://twitter.com/MichaelMarcucc9'
+				/>
 
-        <Icon icon={<MdEmail />} url="mailto:michaelmarcucci23@gmail.com" />
+				<Icon icon={<MdEmail />} url='mailto:michaelmarcucci23@gmail.com' />
 
-        <Icon icon={<FaLinkedinIn />} url="https://www.linkedin.com/in/michael-marcucci-1a577293/" />
-        <Icon icon={<MdFilePresent />} url="" />
+				<Icon
+					icon={<FaLinkedinIn />}
+					url='https://www.linkedin.com/in/michael-marcucci-1a577293/'
+				/>
+				<Icon icon={<MdFilePresent />} url='assets/docs/resume10_22.pdf' />
 
-        {/* <Icon
+				{/* <Icon
           icon={<BsSpotify />}
           url="https://open.spotify.com/user/31l2fev6wn2mtzclbpvzxis3x77q"
         /> */}
-      </div>
-    </div>
-  );
+			</div>
+		</div>
+	);
 };
 
 export default Contact;
+
